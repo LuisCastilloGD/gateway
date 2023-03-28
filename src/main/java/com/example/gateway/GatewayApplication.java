54fab60c7fd25bc39a0c8ebaad9c0048d26230d7
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-28 12:27:17 -0600

    init gateway

---
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+package com.example.gateway;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+					  .route("product_route", r -> r
+							  .path("/product/**")
+							  .uri("http://localhost:8083")
+							)
+					  .build();
+	}
+
+}
